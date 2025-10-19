c210bec8366aa80e6d454c8a7bf3a19a2f644244
quilty128 <quilty.holly@gmail.com>
2025-10-19 18:59:26 -0400

    Implemented day 8 part 2, but it is a mess.

--- src/Day8/Main.hs
+++ src/Day8/Main.hs
@@ -43,8 +43,30 @@ part1 w h =
     . S.filter (\(i, j) -> 0 <= i && i <= h - 1 && 0 <= j && j <= w - 1)
     . H.fold (\nodeList set -> S.union set $ getAntinodes nodeList) S.empty
 
+getAntinodes' :: Int -> Int -> [(Int, Int)] -> Set (Int, Int)
+getAntinodes' w h nodes =
+  foldl'
+    ( \s ((i1, j1), (i2, j2)) ->
+        -- INVARIANT: i1 <= i2, because input was parsed left
+        let (di, dj) = (i2 - i1, j2 - j1)
+            antinodes =
+              (takeWhile inGrid $ iterate (\(i, j) -> (i - di, j - dj)) (i1, j1))
+                ++ (takeWhile inGrid $ iterate (\(i, j) -> (i + di, j + dj)) (i2, j2))
+         in foldl' (\s' antinode -> antinode `S.insert` s') s antinodes
+    )
+    S.empty
+    [(node, node') | (node : rest) <- tails nodes, node' <- rest]
+  where
+    inGrid (i, j) = 0 <= i && i <= h - 1 && 0 <= j && j <= w - 1
+
+part2 :: Int -> Int -> Map Char [(Int, Int)] -> Int
+part2 w h =
+  S.size
+    . H.fold (\nodeList set -> S.union set $ getAntinodes' w h nodeList) S.empty
+
 main :: IO ()
 main = do
-  input <- readFile "input/day8.txt" 
+  input <- readFile "input/day8.txt"
   let (width, height, nodeMap) = parseInput input
   putStrLn $ "Part 1: " ++ (show $ part1 width height nodeMap)
+  putStrLn $ "Part 2: " ++ (show $ part2 width height nodeMap)
